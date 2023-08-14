b89316642146e5aaae1ab0afd5125fc4dd6ac502
микемка <mikemka@vk.com>
2023-08-15 00:52:20 +0500

    change id -> -id

--- shop/main/views.py
+++ shop/main/views.py
@@ -5,7 +5,7 @@ from catalog.models import Category
 def homepage(request):
     template_name = 'main/homepage.html'
     extra = {
-        'categories': Category.objects.all()[:6]
+        'categories': Category.objects.order_by('-id')[:6]
     }
 
     return render(
